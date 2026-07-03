9f99a84ec4386d1a829f6faf4fd3574c966981f3
Kartik Raj <kartikraj@Kartiks-MacBook-Air.local>
2026-07-03 22:34:50 +0530

    Added 10 basic Java programs

---
+++ src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("My name is Kartik Raj.");
+        System.out.println("Welcome to my Java Programs repository.");
+    }
+}
